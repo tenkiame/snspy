bf8c4c741c5e95e644d7a2257e0a987f48ab3a96
tenkiame <52646475+tenkiame@users.noreply.github.com>
2019-07-09 20:00:11 +0900

    Update urls.py

--- contents/urls.py
+++ contents/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'contents'
 
 urlpatterns = [
-    #ここから
     path('create/', views.BlogAddView.as_view(), name='create'),
     path('', views.BlogListView.as_view(), name='list'),
 ]
