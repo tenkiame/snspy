f01b5b638064f929cb0ebaad175d246d4febf19c
tenkiame <52646475+tenkiame@users.noreply.github.com>
2019-07-25 22:53:10 +0900

    Add files via upload

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('', views.Account.as_view(), name='accounts'),
+    path('', views.AccountListView.as_view(), name='accountlist'),
+    path('profile', views.ProfileView.as_view(), name='profile'),
+    path('profile/edit', views.ProfileEditView.as_view(), name='profileedit'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.contrib.auth import get_user_model
+from . import models
+from .models import Profile
 
 
 class SignUpView(CreateView):
@@ -10,7 +12,20 @@ class SignUpView(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'users/signup.html'
 
-class Accounts(ListView):
-    model = get_user_model()
-    fields = ('username', 'email')
-    template_name = 'users/accounts.html'
+class AccountListView(ListView):
+    model = Profile
+    fields = ('username', 'image')
+    template_name = 'users/accountlist.html'
+
+class ProfileView(DetailView):
+    model = Profile
+    fields = ('username', 'user', 'gender', 'birth', 'address', 'phone_number', 'image')
+    pk_url_kwarg = 'profile_pk'
+    template_name = 'users/profile.html'
+
+class ProfileEditView(UpdateView):
+    model = Profile
+    fields = ('username', 'user', 'gender', 'birth', 'address', 'phone_number', 'image')
+    pk_url_kwarg = 'object_pk'
+    success_url = reverse_lazy('accountlist')
+    template_name = 'profileedit.html'
